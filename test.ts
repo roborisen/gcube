9cbf0643b233c9985f681b68ed44b57e44f9f0d2
roborisen <62860479+roborisen@users.noreply.github.com>
2023-10-03 17:49:14 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,12 +1,60 @@
 input.onButtonPressed(Button.A, function () {
-    GCube.setAGCubeSpeed(0, 100)
+    GCube.setAGCubeRotationAngle(0, 360)
     GCube.setAGCubeSpeed(1, 100)
+    GCube.setAGCubeSpeed(2, 50)
 })
-input.onButtonPressed(Button.B, function () {
+input.onGesture(Gesture.LogoUp, function () {
+    GCube.startRollingMatrixImage(1000)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    GCube.setMatrixDisplay(
+        1,
+        "__-__-__-__-__-__-__-__-__",
+        "__-__-__#__-__-__-__-__-__",
+        "__-__#__#__-__-__-__-__-__",
+        "__#__#__#__#__#__#__#__#__",
+        "__#__#__#__#__#__#__#__#__",
+        "__-__#__#__-__-__-__-__-__",
+        "__-__-__#__-__-__-__-__-__",
+        "__-__-__-__-__-__-__-__-__"
+    )
+    GCube.setAGCubeServoAngle(3, 45)
+})
+input.onButtonPressed(Button.AB, function () {
     GCube.stopAllGCubeMotor()
 })
-GCube.waitFirstGCubeConnect()
-GCube.waitAllGCubesConnect(2)
-basic.forever(function () {
-
+input.onButtonPressed(Button.B, function () {
+    GCube.setAGCubeRotationAngle(0, -360)
+    GCube.setAGCubeSpeed(1, -100)
+    GCube.setAGCubeSpeed(2, -50)
 })
+input.onGesture(Gesture.TiltRight, function () {
+    GCube.setMatrixDisplay(
+        1,
+        "__-__-__-__-__-__-__-__-__",
+        "__-__-__-__-__-__#__-__-__",
+        "__-__-__-__-__-__#__#__-__",
+        "__#__#__#__#__#__#__#__#__",
+        "__#__#__#__#__#__#__#__#__",
+        "__-__-__-__-__-__#__#__-__",
+        "__-__-__-__-__-__#__-__-__",
+        "__-__-__-__-__-__-__-__-__"
+    )
+    GCube.setAGCubeServoAngle(3, 135)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    GCube.startShiftingMatrixImage(1)
+})
+GCube.waitFirstGCubeConnect()
+GCube.waitAllGCubesConnect(4)
+GCube.defaultRollingMatrixImage(
+    "A",
+    "__-__-__-__-__-__-__-__-__",
+    "__-__-__-__-__-__-__-__-__",
+    "__-__-__-__#__#__-__-__-__",
+    "__-__-__#__#__#__#__-__-__",
+    "__-__-__#__#__#__#__-__-__",
+    "__-__-__-__#__#__-__-__-__",
+    "__-__-__-__-__-__-__-__-__",
+    "__-__-__-__-__-__-__-__-__"
+)
